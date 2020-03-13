1ce4cd2d957a02436e402584c66ee9f3e0f4da5f
thanapon19 <thanapon@iapp.co.th>
2020-03-13 20:12:31 +0700

    re git

--- appConfig.js
+++ appConfig.js
@@ -1,2 +1,2 @@
-// export const SettingApp =  "https://whereapi.now.sh"
-export const SettingApp =  "http://172.20.10.3:1234" /// แก้ตัวเลขเป็น Ip เครื่องของตัวเอง 
+export const SettingApp =  "https://whereapi-2.pangss.now.sh/"
+// export const SettingApp =  "http://172.20.10.3:1234" /// แก้ตัวเลขเป็น Ip เครื่องของตัวเอง 
